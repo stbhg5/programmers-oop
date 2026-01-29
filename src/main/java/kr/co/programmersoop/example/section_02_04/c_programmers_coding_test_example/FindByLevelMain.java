806f93342c60c79099b19a8085d9a95b88c4e72a
jint <stbhg5@gmail.com>
2026-01-29 22:49:13 +0900

    소스 수정

--- src/main/java/kr/co/programmersoop/example/section_02_04/c_programmers_coding_test_example/FindByLevelMain.java
+++ src/main/java/kr/co/programmersoop/example/section_02_04/c_programmers_coding_test_example/FindByLevelMain.java
@@ -5,21 +5,21 @@ import java.util.List;
 
 public class FindByLevelMain {
     public static void main(String[] args) {
-        String idToFind = "programmers_user1";
+        String idToFind = "real_user1"; // 사용자가 가입하려고 하는 ID
 
+        // 이미 가입된 유저들
         List<User> list = new ArrayList<>();
+        list.add(new User("real_user1", "송진성"));
+        list.add(new User("real_user2", "진트"));
+        list.add(new User("real_user3", "jint"));
 
-        list.add(new User("programmers_user1", "이준형"));
-        list.add(new User("programmers_user2", "푸"));
-        list.add(new User("programmers_user3", "foo"));
-
+        // 아이디 중복 검사
         list.stream()
                 .filter(user -> user.sameId(idToFind))
                 .findAny()
-                .ifPresentOrElse(user -> {
-                    throw new RuntimeException(idToFind + "는 이미 존재하는 아이디입니다.");
-                }, () -> {
-                    System.out.println(idToFind + "는 사용할 수 있는 아이디입니다.");
-                });
+                .ifPresentOrElse(
+                    user -> { throw new RuntimeException(idToFind + "는 이미 존재하는 아이디입니다."); }
+                  , () -> { System.out.println(idToFind + "는 사용할 수 있는 아이디입니다."); }
+                );
     }
 }
